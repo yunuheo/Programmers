59c1cf5a6f6b68dda9645e0b5493c57097e170ff
허윤우 <hyoon516@naver.com>
2022-11-11 15:46:15 +0900

    while문안에 if문 추가
    -입력값이 a로 나눠떨어질때와 그렇지 않을때를 구분해야한다는 것을 파악함.

--- 콜라.py
+++ 콜라.py
@@ -1,26 +1,21 @@
 def solution(a, b, n):
     answer = 0
     temp = 0
-    
-    
-    if a % b == 0:
-        a = a//b
-        b = b//b
         
-    
     if a == n:
         return b
     
     while True:
         answer += (n//a)*b
-        n = n//a
+        n = n%a
         temp += n % a
         if n<a:
-           if (n+temp)%a == 0:
+           if (n+temp) >= a:
                answer += ((n+temp)//a)*b
-           else:
                break
+           
+           break
     return answer
 
-print(solution(2,1,20))
+print(solution(6,2,20))
                
