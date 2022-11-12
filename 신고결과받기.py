609f623d931eb7a5a77e4a8a207f964817ad4c97
허윤우 <hyoon516@naver.com>
2022-11-12 12:59:44 +0900

    Hash자료구조 사용
    -파이썬에서Hash 자료구조는 딕셔너리
    => collections 라이브러리 import해주기 => defaultdict메서드의 매개변수로 value값의 형태를 지정(각각 set, int)

---
+++ 신고결과받기.py
@@ -0,0 +1,11 @@
+import collections
+def solution(id_list, report, k):
+    answer = []
+    
+    report = list(set(report))
+    reportHash = collections.defaultdict(set)
+    stoped = collections.defaultdict(int)
+    
+    
+    
+    return answer
