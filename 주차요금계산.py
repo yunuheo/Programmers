f3577646cfd1bb6960bc33f6c7eeef5d21da11c4
허윤우 <hyoon516@naver.com>
2022-11-15 00:34:35 +0900

    괄호 오류 수정

--- 주차요금계산.py
+++ 주차요금계산.py
@@ -5,14 +5,14 @@ def solution (fees, records):
     isIn = [0]*10000
     sumT = [0]*10000
 
-    for record in recoreds:
+    for record in records:
         a, b, c = record.split() #공백이 구분자
         h, m = a.split(":")
-        if c == 'IN':
-            inTime[int(b)] = int(b)*60+int(m)
+        if c == "IN":
+            inTime[int(b)] = int(h)*60+int(m)
             isIn[int(b)] = 1
         else:
-            sumT[int[b]] += ((int(h))*60+int(m)) - inTime[int(b)]
+            sumT[int(b)] += (int(h)*60+int(m)) - inTime[int(b)]
             isIn[int(b)] = 0
     
     for i in range(10000):
@@ -24,4 +24,6 @@ def solution (fees, records):
             answer.append(fees[1] + max(math.ceil((sumT[i]-fees[0])/fees[2]),0)*fees[3])
 
     return answer
+
+print(solution([180, 5000, 10, 600],["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]))
 
