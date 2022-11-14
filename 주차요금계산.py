cb43c2dff295287364f31830383703ab52b32e6f
허윤우 <hyoon516@naver.com>
2022-11-15 00:02:46 +0900

    차량의 누적주차시간 계산 로직
    -입차시간, 입차상태, 입출차에따른 시간계산(분) 을 담아두는 배열 세개를 생성
    -record 매개변수에 담긴 값을 꺼내어 공백으로 구분

---
+++ 주차요금계산.py
@@ -0,0 +1,22 @@
+import math
+def solution (fees, records):
+    answer = []
+    inTime = [0]*10000
+    isIn = [0]*10000
+    sumT = [0]*10000
+
+    for record in recoreds:
+        a, b, c = record.split() #공백이 구분자
+        h, m = a.split(":")
+        if c == 'IN':
+            inTime[int(b)] = int(b)*60+int(m)
+            isIn[int(b)] = 1
+        else:
+            sumT[int[b]] += ((int(h))*60+int(m)) - inTime[int(b)]
+            isIn[int(b)] = 0
+    
+    for i in range(10000):
+        if isIn[i] == 1:
+            sumT[i] += (23*60+59) - inTime[i]
+
+    return answer
