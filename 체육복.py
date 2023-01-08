e9ff5ee3a4f73abbd153dabf6aa44796551e5dfe
허윤우 <hyoon516@naver.com>
2023-01-08 14:43:13 +0900

    set()을 이용하여 중복된 값을 제거함

--- 체육복.py
+++ 체육복.py
@@ -1,3 +1,21 @@
-list1 = [1,2,3,4,5]
-list2 = [2,3,4,5]
-print(set(list1)-set(list2))
+'''
+문제링크: https://school.programmers.co.kr/learn/courses/30/lessons/42862
+'''
+
+
+def solution(n, lost, reserve):
+    answer = 0
+    #여벌이 있으나 도난당한 학생을 빼주고, 중복된 요소를 없애기 위해 set 자료구조 사용
+    reserve_set = set(reserve) - set(lost)
+    lost_set = set(lost) - set(reserve)
+
+    for i in reserve_set:
+        if i-1 in lost_set:
+            lost_set.remove(i-1) #체육복을 빌리면 lost_set에서 삭제
+        elif i+1 in lost_set:
+            lost_set.remove(i+1) #체육복을 빌리면 lost_set에서 삭제
+    answer = n - len(lost_set)   #전체 학생수에서 여전히 lost_set에 남아 있는 학생수를 빼주자
+    return answer
+
+
+print(solution(5, [2, 4], [3]))
