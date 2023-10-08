04c699fcba1d509fc79f6b77b47169d15892e616
heoyunu <hyoon516@naver.com>
2023-10-09 01:00:45 +0900

    check변수에 큐로부터 pop된 값을 담으면서 내부 for문이 동적으로 전체길이를 순회하도록 함

--- 문제집/주시가격.py
+++ 문제집/주시가격.py
@@ -1,5 +1,20 @@
 #문제링크: https://school.programmers.co.kr/learn/courses/30/lessons/42584
 
+from collections import deque
+
 def solution(prices):
     answer = []
+    queue = deque(prices)
+    
+    while queue:
+        
+        check = queue.popleft()
+
+        sec = 0   
+        for q in queue:  #마지막 pop 연산 후 빈큐 이므로 수행되지 않음
+            sec += 1
+            if check > q:
+                break
+        answer.append(sec)
+        
     return answer
